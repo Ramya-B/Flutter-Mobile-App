8a11da874fe1b75c6d6a389c5327cb29f19f3a3b
mnettem <manohar.nettem@tradeleaves.com>
2020-01-28 10:15:34 +0530

    initial chages

--- mobile_samples/lib/main.dart
+++ mobile_samples/lib/main.dart
@@ -1,111 +1,134 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MaterialApp(
+      home: Sample(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+class Sample extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                child: Image.asset(
+                  'assets/tl.png',
+                  fit: BoxFit.contain,
+                  height: 25,
+                ),
+                backgroundColor: Colors.white,
+                radius: 15,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'TRADELEAVES'
+                  ))
+            ],
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            new IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            new IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {})
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        drawer: new Drawer(
+          child: new ListView(
+            children: <Widget>[
+              Container(
+                child: new UserAccountsDrawerHeader(
+                  accountName: Text('Manohar Nettem'),
+                  accountEmail: Text('manohar.nettem@gmail.com'),
+                  currentAccountPicture: GestureDetector(
+                    child: new CircleAvatar(child: Icon(Icons.person)
+//                   Image(image: AssetImage('assets/manohar.jpg')) ,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Home Page'),
+                  leading: Icon(Icons.home),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Profile'),
+                  leading: Icon(Icons.person),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Categories'),
+                  leading: Icon(Icons.dashboard),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Favourites'),
+                  leading: Icon(Icons.favorite),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Orders'),
+                  leading: Icon(Icons.shopping_basket),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Notifications'),
+                  leading: Icon(Icons.notifications),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('Settings'),
+                  leading: Icon(Icons.settings),
+                ),
+              ),
+              InkWell(
+                child: new ListTile(
+                  title: Text('About'),
+                  leading: Icon(Icons.info),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(8.0),
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(20, (index) {
+            return Card(
+              child: Center(
+                child: Text(
+                  'Item $index',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headline,
+                ),
+              ),
+            );
+          }),
+        ),
+        floatingActionButton: FloatingActionButton(
+        child: Text('chat'),
+    backgroundColor: Colors.green,
+    ),
     );
   }
 }
