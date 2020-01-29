d13c978e08c34c3eb64e88e3ec5fc00a2247a096
Ramya B <ramya.b@tradeleaves.com>
2020-01-29 14:36:23 +0530

    changes for drawer

--- mobile_samples/lib/main.dart
+++ mobile_samples/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tradeleaves/components/products/ProductsList.dart';
 import 'package:tradeleaves/components/CustomAppBar.dart';
 import 'package:tradeleaves/components/Profile/Profile.dart';
 import 'package:tradeleaves/components/CustomBottomNavigationBar.dart';
+import 'package:tradeleaves/components/CustomDrawer.dart';
 
 void main() => runApp(MaterialApp(
       home: Sample(),
@@ -15,91 +16,92 @@ class Sample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomToolBar(),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            Container(
-              child: new UserAccountsDrawerHeader(
-                accountName: Text('Manohar Nettem'),
-                accountEmail: Text('manohar.nettem@gmail.com'),
-                currentAccountPicture: GestureDetector(
-                  child: new CircleAvatar(child: Icon(Icons.person)
-                      ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('Home Page'),
-                leading: Icon(Icons.home),
-              ),
-            ),
-            InkWell(
-              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-                  builder: (context) => new Profile(
-                      userId: 'manohar',
-                      companyName:'my dream company',
-                      emailId:'manohar.nettem@gmail.com',
-                      phoneNo: 7799867839,
-                      firstName: 'Manohar',
-                      lastName: 'Nettem'
-                  ))),
-
-
-              child: new ListTile(
-                title: Text('Profile'),
-                leading: Icon(Icons.person),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('Categories'),
-                leading: Icon(Icons.dashboard),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('Favourites'),
-                leading: Icon(Icons.favorite),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('Orders'),
-                leading: Icon(Icons.shopping_basket),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('Notifications'),
-                leading: Icon(Icons.notifications),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('Settings'),
-                leading: Icon(Icons.settings),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: new ListTile(
-                title: Text('About'),
-                leading: Icon(Icons.info),
-              ),
-            )
-          ],
-        ),
-      ),
+      drawer: CustomDrawer(),
+//      new Drawer(
+//        child: new ListView(
+//          children: <Widget>[
+//            Container(
+//              child: new UserAccountsDrawerHeader(
+//                accountName: Text('Manohar Nettem'),
+//                accountEmail: Text('manohar.nettem@gmail.com'),
+//                currentAccountPicture: GestureDetector(
+//                  child: new CircleAvatar(child: Icon(Icons.person)
+//                      ),
+//                ),
+//                decoration: BoxDecoration(
+//                  color: Colors.green,
+//                ),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('Home Page'),
+//                leading: Icon(Icons.home),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+//                  builder: (context) => new Profile(
+//                      userId: 'manohar',
+//                      companyName:'my dream company',
+//                      emailId:'manohar.nettem@gmail.com',
+//                      phoneNo: 7799867839,
+//                      firstName: 'Manohar',
+//                      lastName: 'Nettem'
+//                  ))),
+//
+//
+//              child: new ListTile(
+//                title: Text('Profile'),
+//                leading: Icon(Icons.person),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('Categories'),
+//                leading: Icon(Icons.dashboard),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('Favourites'),
+//                leading: Icon(Icons.favorite),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('Orders'),
+//                leading: Icon(Icons.shopping_basket),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('Notifications'),
+//                leading: Icon(Icons.notifications),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('Settings'),
+//                leading: Icon(Icons.settings),
+//              ),
+//            ),
+//            InkWell(
+//              onTap: () {},
+//              child: new ListTile(
+//                title: Text('About'),
+//                leading: Icon(Icons.info),
+//              ),
+//            )
+//          ],
+//        ),
+//      ),
       bottomNavigationBar: CustomNavBar(),//
       body: ListView(
         children: <Widget>[
